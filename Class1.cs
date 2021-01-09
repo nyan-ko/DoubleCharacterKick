fc9f0b3c991733ece5dbb155ff004062dc31eaf9
nyan-ko <dumbstupidcatboy@gmail.com>
2021-01-09 08:46:09 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleCharacterKick
+{
+    public class Class1
+    {
+    }
+}
